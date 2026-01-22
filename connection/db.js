58439867755603f8acb84b18ebae697bba3da1eb
Vikesh Hinge <103572278+VikeshHinge@users.noreply.github.com>
2026-01-22 22:27:13 +0530

    Clear MongoDB connection string in db.js
    Removed sensitive connection string from db.js.

--- connection/db.js
+++ connection/db.js
@@ -1,4 +1,4 @@
 const mongoose = require ('mongoose')
-const connection = mongoose.connect(`mongodb+srv://vikesh:sector4e90@cluster0.tiwvvgy.mongodb.net/meanbuy?retryWrites=true&w=majority`)
+const connection = mongoose.connect(``)
 module.exports = {connection}
 
